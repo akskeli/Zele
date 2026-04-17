60a9199181f5874b268518dd2419e4381059f248
akskeli <akuzmani@fesb.hr>
2026-04-17 14:38:30 +0200

    Add files via upload

---
+++ grupaA_zadatak_3.c
@@ -0,0 +1,49 @@
+/*GRUPA A - zadatak 3*/
+
+#include <stdio.h>
+
+int main() {
+    int n, broj, original, obrnuti, ostatak;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &broj);
+
+        original = broj;
+        obrnuti = 0;
+
+        while(broj != 0) {
+            ostatak = broj % 10;
+            obrnuti = obrnuti * 10 + ostatak;
+            broj = broj / 10;
+        }
+
+        if(original == obrnuti) {
+            printf("Palindrom\n");
+        } else {
+            printf("Nije palindrom\n");
+        }
+    }
+
+    return 0;
+}
+
+
+/*  testiraj s primjerom
+Ulaz:
+5
+121
+123
+44
+567
+9
+
+Izlaz:
+Palindrom
+Nije palindrom
+Palindrom
+Nije palindrom
+Palindrom
+*/
+
