b9efd05dcfcb6ef266a7a3bf4676126e4436fef9
akskeli <akuzmani@fesb.hr>
2026-04-17 14:52:27 +0200

    Add files via upload

---
+++ grupaB_zadatak_4.c
@@ -0,0 +1,25 @@
+/*GRUPA B - zadatak 4*/
+
+#include <stdio.h>
+
+int main() {
+    int broj, a, b, obrnut;
+
+    for(broj = 10; broj <= 99; broj++) {
+
+        if(broj % 2 != 0) {
+
+            a = broj / 10;
+            b = broj % 10;
+
+            obrnut = b * 10 + a;
+
+            if(obrnut % 2 != 0) {
+                printf("%d\n", broj);
+            }
+        }
+    }
+
+    return 0;
+}
+
