8c09ef70671644883e55b12baff0bce8555f3c66
akskeli <akuzmani@fesb.hr>
2026-04-17 14:50:24 +0200

    Add files via upload

---
+++ grupaB_zadatak_3.c
@@ -0,0 +1,42 @@
+/*GRUPA B - zadatak 3*/
+
+#include <stdio.h>
+
+int main() {
+    double a, rezultat = 1;
+    int n;
+
+    scanf("%lf %d", &a, &n);
+
+    for(int i = 0; i < n; i++) {
+        rezultat = rezultat * a;
+    }
+
+    printf("%.2f", rezultat);
+
+    return 0;
+}
+
+
+/*
+Ulaz:
+2 5
+
+Izlaz:
+32.00
+-----------------------------
+
+Ulaz:
+3 3
+
+Izlaz:
+9.00
+-----------------------------
+
+Ulaz:
+5 0
+
+Izlaz:
+1.00
+-----------------------------
+*/
