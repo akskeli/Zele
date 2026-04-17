d2283475754e40ff53da6282f1f2ba8d549c6ec0
akskeli <akuzmani@fesb.hr>
2026-04-17 14:42:13 +0200

    Add files via upload

---
+++ grupaA_zadatak_4.c
@@ -0,0 +1,24 @@
+/*GRUPA A - zadatak 4*/
+
+#include <stdio.h>
+
+int main() {
+    int broj, a, b, c, suma;
+
+    for(broj = 100; broj <= 999; broj++) {
+        a = broj / 100;
+        b = (broj / 10) % 10;
+        c = broj % 10;
+
+        suma = a*a*a + b*b*b + c*c*c;
+
+        if(broj == suma) {
+            printf("%d\n", broj);
+        }
+    }
+
+    return 0;
+}
+
+
+
